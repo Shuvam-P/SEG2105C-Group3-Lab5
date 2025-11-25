4d046de897c3a5b4e248adc2cbcaea0e42dfdf1e
Shuvam Pathak <Shuvamp.ola@gmail.com>
2025-11-24 23:43:31 -0500

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityResultLauncher<Intent> avatarLauncher;
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView avatar = findViewById(R.id.imgAvatar);
+
+        // ActivityResultLauncher setup
+        avatarLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent data = result.getData();
+                        int imgId = data.getIntExtra("img_id", -1);
+
+                        ImageView img = findViewById(R.id.imgAvatar);
+
+                        switch (imgId) {
+                            case R.id.img0: img.setImageResource(R.drawable.flag_00); break;
+                            case R.id.img1: img.setImageResource(R.drawable.flag_01); break;
+                            case R.id.img2: img.setImageResource(R.drawable.flag_02); break;
+                            case R.id.img3: img.setImageResource(R.drawable.flag_03); break;
+                            case R.id.img4: img.setImageResource(R.drawable.flag_04); break;
+                            case R.id.img5: img.setImageResource(R.drawable.flag_05); break;
+                            case R.id.img6: img.setImageResource(R.drawable.flag_06); break;
+                            case R.id.img7: img.setImageResource(R.drawable.flag_07); break;
+                            case R.id.img8: img.setImageResource(R.drawable.flag_08); break;
+                        }
+                    }
+                }
+        );
+    }
+
+    // --- Open the Avatar Selection Activity ---
+    public void OnSetAvatarButton(View v) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        avatarLauncher.launch(intent);
+    }
+
+    // --- Open in Google Maps ---
+    public void OnOpenInGoogleMaps(View v) {
+        EditText postal = findViewById(R.id.editPostal);
+        String address = postal.getText().toString();
+
+        Uri uri = Uri.parse("geo:0,0?q=" + Uri.encode(address));
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.setPackage("com.google.android.apps.maps");
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+    public void SetTeamIcon(View v) {
+        Intent result = new Intent();
+        result.putExtra("img_id", v.getId());
+        setResult(RESULT_OK, result);
+        finish();
+    }
+}
